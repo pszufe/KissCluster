f56b0564ad876dd1a94d8a528500def5b9d939a6
Przemyslaw Szufel <pszufe@gmail.com>
2017-07-30 13:31:20 +0200

    update

---
+++ src/create_cluster_footer.sh
@@ -0,0 +1,14 @@
+tmpname=`tempfile`
+printf ${job_envelope_base64} | base64 -d > ${tmpname}
+aws s3 --region ${REGION} mv ${tmpname} ${S3_LOCATION}/app/job_envelope.sh
+
+printf "#!/bin/bash\n\n" > ${RUN_NODE_FILE}
+printf "CLUSTERNAME=${CLUSTERNAME}\n" >> ${RUN_NODE_FILE}
+printf "REGION=${REGION}\n" >> ${RUN_NODE_FILE}
+printf "\n" >> ${RUN_NODE_FILE}
+printf ${run_node_template_base64} | base64 -d >> ${RUN_NODE_FILE}
+
+printf "\nSUCCESS!\n"
+printf "The cluster ${CLUSTERNAME} has been successfully build!  \n"
+printf "Now you can simply run ${RUN_NODE_FILE} on any AWS EC2 machine to start processing on your cluster. \n"
+printf "${RUN_NODE_FILE} can also be used as a cloud-init configuration for EC2 instances. \n"
