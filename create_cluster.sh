279d50b78c24b69e7ddaa45d985c541b1b55071b
Ubuntu <ubuntu@ip-172-31-29-208.us-east-2.compute.internal>
2017-07-30 20:11:05 +0000

    .

--- create_cluster.sh
+++ create_cluster.sh
@@ -251,20 +251,17 @@ tmpname=`tempfile`
 printf "${job_envelope_base64}" | base64 -d > ${tmpname}
 aws s3 --region ${REGION} mv ${tmpname} ${S3_LOCATION}/app/job_envelope.sh
 
+S3_RUN_NODE_SCRIPT=${S3_LOCATION}/app/run_node_${CLUSTERNAME}.sh
+
 printf "#!/bin/bash\n\n" > ${tmpname}
 printf "CLUSTERNAME=${CLUSTERNAME}\n" >> ${tmpname}
-printf "REGION=${REGION}\n\n" >> ${tmpname}
-
-
-S3_RUN_NODE_SCRIPT=${S3_LOCATION}/app/run_node_${CLUSTERNAME}.sh
+printf "REGION=${REGION}\n" >> ${tmpname}
 printf "${run_node_template_base64}" | base64 -d >> ${tmpname}
 aws s3 --region ${REGION} mv ${tmpname} ${S3_RUN_NODE_SCRIPT}
-
 
 printf "#!/bin/bash\n\n" > ${CLOUD_INIT_FILE}
 printf "CLUSTERNAME=${CLUSTERNAME}\n" >> ${CLOUD_INIT_FILE}
 printf "S3_RUN_NODE_SCRIPT=${S3_RUN_NODE_SCRIPT}\n\n" >> ${CLOUD_INIT_FILE}
-
 
 printf  "${cloud_init_base64}" | base64 -d >> ${CLOUD_INIT_FILE}
 chmod +x ${CLOUD_INIT_FILE}
