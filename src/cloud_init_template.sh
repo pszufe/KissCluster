8f107ecbb557ecead64b574a9ef1ac0116dacf9d
Przemyslaw Szufel <pszufe@gmail.com>
2017-08-08 03:03:11 +0200

    typo

--- src/cloud_init_template.sh
+++ src/cloud_init_template.sh
@@ -2,8 +2,11 @@
 
 sudo apt update --yes
 sudo apt install awscli jq --yes
-sudo su ubuntu
+sudo su - ubuntu
 
-aws  --region ${REGION} s3 cp ${S3_RUN_NODE_SCRIPT} /home/ubuntu/run_node_${CLUSTERNAME}.sh
+aws  --region ${REGION} s3 cp ${S3_RUN_NODE_SCRIPT} /home/ubuntu/run_node_${CLUSTERNAME}.sh 
 cd /home/ubuntu
+
+echo "Now running the command"
+echo "bash run_node_${CLUSTERNAME}.sh ${REGION} ${CLUSTERNAME}"
 bash run_node_${CLUSTERNAME}.sh ${REGION} ${CLUSTERNAME}
