ee0015c0323f4e92b5704827deb8f9ddfbdd0f92
Ubuntu <ubuntu@ip-172-31-35-72.us-east-2.compute.internal>
2017-09-10 00:41:41 +0000

    newparms

--- kissc.sh
+++ kissc.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+COMMAND=$1
+
+function contains {
+    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
+}
+
+if [[ -z $COMMAND ]]; then
+
+   echo "usage: kissc.sh <command> [parameters] clustername@aws-region"
+   echo "KISSC: error: the following argument is required: command"
+   echo "The following commands are available:"
+   echo "create : Creates a cluster in the given region with the given name. If the cluster already exists its data will be reset."
+   echo "submit : Submits a job to the cluster"
+   echo "Run kissc.sh <command> help to see help for a specific command"
+   echo "kissc: error: the following arguments are required: command"
+   exit 1
+fi
+
+
+function usage {
+    ERROR=$1
+    echo "Usage:"
+    echo "kissc.sh --region region --command command --folder folder --s3_bucket s3_bucket clustername"
+    echo ""
+    echo "--region region - AWS region where the computations will be carried out (optional parameter, if not given defaults to us-east-1)"
+    echo "--command - command to be executed on each node. \
+        Note that an additional parameter jobid will be added \
+        to each command executed on the clustername. \
+        The command will be executed on nodes within the folder given as the next parameter"
+    echo "--folder folder - a local path  that contains all files needed to execute the command. \
+        The contents of the folder will be copied to each cluster node"
+    echo "--min_jobid minjobid - starting job id - an optional parameter, requires command and folder parameters"
+	echo "--max_jobid maxjobid - ending job id - an optional parameter, requires command and folder parameters"
+	echo "--s3_bucket s3_bucket - name of an AWS S3 bucket (e.g. s3://mybucketname/) that will be used \
+        to store cluster data and will be used for result collection (mandatory parameter)"
+	echo "--queue_name queue_name a label that will describe a particular queue created if the command parameter is given (optional)"
+    echo "clustername - name of your cluster (mandatory parameter)"
+    echo "Note: the parameters command and folder are optional, but if one is present the other also should be given."
+    exit $ERROR
+}
+
 function checkinstall {
   PKG_NAME=$1
   PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${PKG_NAME}|grep "install ok installed")
@@ -17,7 +58,7 @@ set -e
 
 HELP=0
 ERROR=0
-REGION=us-east-1
+REGION=""
 
 CLUSTERNAME=""
 COMMAND=""
@@ -31,10 +72,6 @@ while [[ $# -gt 1 ]]
 do
 key="$1"
 case $key in
-    -r|--region)
-    REGION="$2"
-	shift
-    ;;
     -c|--command)
     COMMAND="$2"
 	shift
@@ -42,11 +79,11 @@ case $key in
     -f|--folder)
     HOME_DIR="$2"
 	shift
-    ;;	
+    ;;
 	-s|--min_jobid)
     MINJOBID="$2"
 	shift
-    ;;	
+    ;;
 	-e|--max_jobid)
     MAXJOBID="$2"
 	shift
@@ -55,15 +92,15 @@ case $key in
     S3="$2"
 	shift
     ;;
-    -h|--help)    
-    HELP=1    
+    -h|--help)
+    HELP=1
     ;;
 	-n|--queue_name)
     QUEUE_NAME="$2"
 	shift
-    ;;	
+    ;;
     *)
-    ERROR=1  
+    ERROR=1
 	echo "Error: Unknown option $key"
 	break;
     ;;
@@ -77,7 +114,6 @@ if [[ -n $1 ]]; then
 	if [[ -z ${QUEUE_NAME} ]]; then
 	   QUEUE_NAME=${CLUSTERNAME}
 	fi
-	
 else
 	ERROR=1
 	echo "Error: The last parameter (clustername) not given"
@@ -90,24 +126,24 @@ fi
 if [[ $HELP = "0" ]] && [[ $ERROR = "0" ]]; then
     if [[ -z $S3 ]]; then
         echo "Error: missing --s3_bucket parameter"
-        ERROR=1    
+        ERROR=1
     fi
     if [[ -z $COMMAND ]] && [[ ! -z $HOME_DIR ]]; then
         echo "Error: The application folder ($HOME_DIR) is given but no command to be executed"
-        ERROR=1    
+        ERROR=1
     fi
     if [[ ! -z $COMMAND ]] && [[ -z $HOME_DIR ]]; then
         echo "Error: The command to be executed ($COMMAND) is given but no application folder"
-        ERROR=1    
-    fi	
+        ERROR=1
+    fi
 	if [[ ! -z "${MINJOBID}${MAXJOBID}" ]] && [[ -z $COMMAND ]]; then 
         echo "Error: The job range is given but no command to be executed"
-        ERROR=1    
-	fi 	
+        ERROR=1
+	fi
 	if [[ ! -z "${MINJOBID}${MAXJOBID}" ]] && [[ -z $HOME_DIR ]]; then 
         echo "Error: The job range is given but no application folder"
-        ERROR=1    
-	fi 
+        ERROR=1
+	fi
     echo ERROR $ERROR
     echo HELP $HELP
     echo REGION $REGION
@@ -119,25 +155,7 @@ fi
 
 
 if [[ $HELP = "1" ]] || [[ $ERROR = "1" ]]; then
-    echo "Usage:"
-    echo "kissc-start.sh --region region --command command --folder folder --s3_bucket s3_bucket clustername"
-    echo ""
-    
-    echo "--region region - AWS region where the computations will be carried out (optional parameter, if not given defaults to us-east-1)"
-    echo "--command - command to be executed on each node. \
-        Note that an additional parameter jobid will be added \
-        to each command executed on the clustername. \
-        The command will be executed on nodes within the folder given as the next parameter"
-    echo "--folder folder - a local path  that contains all files needed to execute the command. \
-        The contents of the folder will be copied to each cluster node"
-    echo "--min_jobid minjobid - starting job id - an optional parameter, requires command and folder parameters"
-	echo "--max_jobid maxjobid - ending job id - an optional parameter, requires command and folder parameters"
-	echo "--s3_bucket s3_bucket - name of an AWS S3 bucket (e.g. s3://mybucketname/) that will be used \
-        to store cluster data and will be used for result collection (mandatory parameter)"
-	echo "--queue_name queue_name a label that will describe a particular queue created if the command parameter is given (optional)"
-    echo "clustername - name of your cluster (mandatory parameter)"
-    echo "Note: the parameters command and folder are optional, but if one is present the other also should be given."
-    exit $ERROR
+    usage $ERROR
 fi
 
 
@@ -153,12 +171,12 @@ CLOUD_INIT_FILE=./cloud_init_node_${CLUSTERNAME}.sh
 
 function wait4table {
     TABLENAME=$1
-    while  	    
+    while
         status=`aws dynamodb --region ${REGION} describe-table --table-name ${TABLENAME} 2>/dev/null  | jq -r ".Table.TableStatus"`
 		if [[ $status != "ACTIVE" ]]; then
 			echo "Waiting for DynamoDB table ${TABLENAME} in region ${REGION} to be active"
 			sleep 3
-		fi        
+		fi
         [[ $status != "ACTIVE" ]]
     do
         :
@@ -172,7 +190,7 @@ function droptable {
 	if [[ ! -z "${res// }" ]]; then
 	  echo "Dropping DynamoDB table ${TABLENAME}"
 	  res=`aws dynamodb --region ${REGION}  delete-table --table-name ${TABLENAME}`
-	  while  
+	  while
 	    res=`aws dynamodb --region ${REGION} describe-table --table-name ${TABLENAME} 2>/dev/null`
 		echo "Waiting for DynamoDB table ${TABLENAME} to be dropped"
 		sleep 3
@@ -209,9 +227,6 @@ res=`aws dynamodb --region ${REGION} put-item --table-name kissc_clusters \
 		   "S3_folder":{"S":"'${S3_LOCATION}'"}, "date":{"S":"'${createddate}'"},\
 		   "creator":{"S":"'${USER}'@'${HOSTNAME}'"}}  '`
 
-		   
-		   
-		   
 
 
 droptable ${NODESTABLE}
@@ -225,13 +240,12 @@ res=`aws dynamodb --region ${REGION}  create-table --table-name ${NODESTABLE} \
     --key-schema AttributeName=nodeid,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1`
 
-	
+
 echo "Creating DynamoDB table ${QUEUESTABLE}"
 res=`aws dynamodb --region ${REGION}  create-table --table-name ${QUEUESTABLE} \
     --attribute-definitions AttributeName=queueid,AttributeType=N \
     --key-schema AttributeName=queueid,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5`
-	
 
 echo "Creating DynamoDB table ${JOBSTABLE}"
 res=`aws dynamodb --region ${REGION}  create-table --table-name ${JOBSTABLE} \
@@ -254,9 +268,7 @@ if [[ ! -z ${COMMAND} ]];then
     bash ${DIR}/kissc-qsub.sh --region ${REGION} --command "${COMMAND}" \
 		--folder "${HOME_DIR}" --s3_bucket ${S3} --queue_name "${QUEUE_NAME}" \
 		--min_jobid ${MINJOBID} --max_jobid ${MAXJOBID} "${CLUSTERNAME}"
-	
 fi
-    
 
 
 
