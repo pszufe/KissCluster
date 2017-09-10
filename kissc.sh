ce17c243f99cb32f58d3c9f0850f74f8c8708a52
Ubuntu <ubuntu@ip-172-31-35-72.us-east-2.compute.internal>
2017-09-10 18:58:54 +0000

    a

--- kissc.sh
+++ kissc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-COMMAND=$1
 
 function contains {
     [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
@@ -9,64 +8,87 @@ function contains {
 function basic_usage {
    echo "usage: kissc.sh <command> [parameters] clustername@region"
    echo "region : AWS region where the cluster information will be stored (e.g. us-east-1)"
-   echo "kissc: error: $2"
+   if [[ -n "$2" ]]; then
+     echo "kissc: error: $2"
+   fi
    echo "The following commands are available:"
    echo "create : Creates a cluster in the given region with the given name. If the cluster already exists its data will be reset."
    echo "submit : Submits a job to the cluster"
    echo "delete : Deletes a cluster"
    echo "Run kissc.sh <command> help to see help for a specific command"
-   echo "kissc: error: $2"
+   if [[ -n "$2" ]]; then
+     echo "kissc: error: $2"
+   fi
    exit $1
 }
 
 function usage_create {
+    if [[ -n "$2" ]]; then
+        echo "kissc create: error: $2"
+    fi
     echo "Usage:"
     echo "kissc.sh create [parameters] clustername@region"
-    echo ""
-    echo "--passwordless_ssh - key name that will be used to configure passwordless ssh across cluster nodes. \
-        Note that an additional parameter jobid will be added \
-        to each command executed on the clustername. \
-        The command will be executed on nodes within the folder given as the next parameter"
+    echo "Supported parameters:"
+    echo "--passwordless_ssh keyname - key name that will be used to configure passwordless ssh across cluster nodes. "
+    echo "--user username - username that will be used on cluster nodes, defaults to 'ubuntu'"
     echo "clustername@region - name and region of your cluster"
+    if [[ -n "$2" ]]; then
+        echo "kissc: error: $2"
+    fi
     exit $1
 }
 
 function usage_delete {
+    if [[ -n "$2" ]]; then
+        echo "kissc delete: error: $2"
+    fi
     echo "Usage:"
     echo "kissc.sh delete clustername@region"
     echo "Deletes the given cluster information."
+    echo "This operation does not have additional parameters."
     echo "Note that the function call does try to terminate the nodes in any way!"
-	echo "If you run your nodes in AWS you should try to terminate them manually"	     
+    echo "If you run your nodes in AWS you should try to terminate them manually"
     echo "clustername@region - name and region of your cluster"
+    if [[ -n "$2" ]]; then
+        echo "kissc: error: $2"
+    fi
     exit $1
 }
 
 
 function usage_submit {
+    if [[ -n "$2" ]]; then
+        echo "kissc submit: error: $2"
+    fi
     echo "Usage:"
-    echo "kissc.sh submit --command command --folder folder --s3_bucket s3_bucket [other parameters] clustername@region"
+    echo "kissc.sh submit --bash_command bash_command --folder folder --s3_bucket s3_bucket [other parameters] clustername@region"
     echo ""
-    echo "--command - command to be executed on each node. \
-        Note that an additional parameter jobid will be added \
-        to each command executed on the clustername. \
-        The command will be executed on nodes within the folder given as the next parameter"
-    echo "--folder folder - a local path  that contains all files needed to execute the command. \
-        The contents of the folder will be copied to each cluster node"
-	echo "--s3_bucket s3_bucket - name of an AWS S3 bucket (e.g. s3://mybucketname/) that will be used \
-        to store cluster data and will be used for result collection (mandatory parameter)"
-	echo "--min_jobid minjobid - starting job id - an optional parameter, requires command and folder parameters"
-	echo "--max_jobid maxjobid - ending job id - an optional parameter, requires command and folder parameters"
-	echo "--queue_name queue_name a label that will describe a particular queue created if the command parameter is given (optional)"
+    echo "Supported parameters:"
+    echo "--bash_command bash_command - command to be executed on each node. "
+    echo "  Note that an additional parameter jobid will be added "
+    echo "  to each command executed on the clustername. "
+    echo "  The command will be executed on nodes within the folder given as the next parameter"
+    echo "--folder folder - a local path  that contains all files needed to execute the command. "
+    echo "  The contents of the folder will be copied to each cluster node"
+    echo "--s3_bucket s3_bucket - name of an AWS S3 bucket (e.g. s3://mybucketname/) that will be used "
+    echo "  to store cluster data and will be used for result collection (mandatory parameter)"
+    echo "--min_jobid minjobid - starting job id - an optional parameter, requires command and folder parameters"
+    echo "--max_jobid maxjobid - ending job id - an optional parameter, requires command and folder parameters"
+    echo "--queue_name queue_name a label that will describe a particular queue created if the command parameter is given (optional)"
     echo "clustername@region - name and region of your cluster"
+    if [[ -n "$2" ]]; then
+        echo "kissc: error: $2"
+    fi
     exit $1
 }
 
+COMMAND=$1
 
 if [[ -z $COMMAND ]] || ! `contains "create submit delete" $COMMAND`; then
-   basic_usage 1 "the following arguments are required: command"  
+   basic_usage 1 "the following arguments are required: command"
 fi
 
-if [[ "$3" == "help" ]]; then
+if [[ "$2" == "help" ]]; then
 	if [[ $COMMAND = "create" ]]; then
 	  usage_create 0
 	elif [[ $COMMAND = "submit" ]]; then
@@ -78,24 +100,25 @@ if [[ "$3" == "help" ]]; then
 fi
 
 
-HELP=0
-ERROR=0
 REGION=""
 CLUSTERNAME=""
-COMMAND=""
+BASH_COMMAND=""
 HOME_DIR=""
 S3=""
 MINJOBID=1
 MAXJOBID=1000000000
 QUEUE_NAME=""
 KEY_NAME=""
+USERNAME=ubuntu
 
+shift
+# shifts skips the <command> parameter
 while [[ $# -gt 1 ]]
 do
 key="$1"
 case $key in
     -c|--command)
-    COMMAND="$2"
+    BASH_COMMAND="$2"
 	shift
     ;;
     -f|--folder)
@@ -117,14 +140,18 @@ case $key in
     -h|--help)
     HELP=1
     ;;
-	-n|--queue_name)
+    -n|--queue_name)
     QUEUE_NAME="$2"
 	shift
     ;;
-	-p|--passwordless_ssh)
+    -p|--passwordless_ssh)
     KEY_NAME="$2"
 	shift
-    ;;	
+    ;;
+    -u|--user)
+    USERNAME="$2"
+	shift
+    ;;
     *)
     ERROR=1
 	echo "Error: Unknown option $key"
@@ -141,58 +168,13 @@ if [[ -n $1 ]]; then
 	REGION=${vals[1]}
     if [[ -z ${REGION} ]]; then
 	   basic_usage 1 "The last parameter does not contain region name. Should be clustername@regionname"
-    fi	
-	
+    fi
 	if [[ -z ${QUEUE_NAME} ]]; then
 	   QUEUE_NAME=${CLUSTERNAME}
 	fi
 else
 	basic_usage 1 "The last parameter (clustername@region) not given"
 fi
-
-echo "OK!"
-exit 0
-
-
-if [[ $CLUSTERNAME = "-h" ]] || [[ $CLUSTERNAME = "--help" ]]; then
-    HELP=1
-fi
-
-if [[ $HELP = "0" ]] && [[ $ERROR = "0" ]]; then
-    if [[ -z $S3 ]]; then
-        echo "Error: missing --s3_bucket parameter"
-        ERROR=1
-    fi
-    if [[ -z $COMMAND ]] && [[ ! -z $HOME_DIR ]]; then
-        echo "Error: The application folder ($HOME_DIR) is given but no command to be executed"
-        ERROR=1
-    fi
-    if [[ ! -z $COMMAND ]] && [[ -z $HOME_DIR ]]; then
-        echo "Error: The command to be executed ($COMMAND) is given but no application folder"
-        ERROR=1
-    fi
-	if [[ ! -z "${MINJOBID}${MAXJOBID}" ]] && [[ -z $COMMAND ]]; then 
-        echo "Error: The job range is given but no command to be executed"
-        ERROR=1
-	fi
-	if [[ ! -z "${MINJOBID}${MAXJOBID}" ]] && [[ -z $HOME_DIR ]]; then 
-        echo "Error: The job range is given but no application folder"
-        ERROR=1
-	fi
-    echo ERROR $ERROR
-    echo HELP $HELP
-    echo REGION $REGION
-    echo CLUSTERNAME $CLUSTERNAME
-    echo COMMAND $COMMAND
-    echo HOME_DIR $HOME_DIR
-    echo S3 $S3
-fi
-
-
-if [[ $HELP = "1" ]] || [[ $ERROR = "1" ]]; then
-    usage $ERROR
-fi
-
 
 
 
@@ -214,10 +196,23 @@ checkinstall awscli
 
 
 if [[ $COMMAND = "create" ]]; then
-  echo create
+  if [[ -n $KEY_NAME ]];then
+     KEY_FILE=~/.ssh/$KEY_NAME
+     echo "Creating a key $KEY_NAME for passwordless SSH in file $KEY_FILE"
+     ssh-keygen -P "" -t rsa -f $KEY_FILE
+     printf "\nUser $USERNAME\nPubKeyAuthentication yes\nIdentityFile $KEY_NAME\n" >> ~/.ssh/config
+  fi
 
 elif [[ $COMMAND = "submit" ]]; then
-  echo submit
+  if [[ -z $S3 ]]; then
+      usage_submit 1 "missing --s3_bucket parameter"
+  fi
+  if [[ -z $BASH_COMMAND ]]; then
+      usage_submit 1 "missing --bash_command parameter"
+  fi
+  if [[ -z $HOME_DIR ]]; then
+      usage_submit 1 "missing --folder parameter"
+  fi
 
 elif [[ $COMMAND = "delete" ]]; then
    echo delete
@@ -226,12 +221,7 @@ fi
 exit 0
 
 
-
-
-
-
 S3_LOCATION=${S3}/${CLUSTERNAME}
-
 JOBSTABLE="kissc_jobs_${CLUSTERNAME}"
 QUEUESTABLE="kissc_queues_${CLUSTERNAME}"
 NODESTABLE="kissc_nodes_${CLUSTERNAME}"
