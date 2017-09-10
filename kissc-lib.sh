618819c5da4c917588d72b6d76d3240aa09034c9
Przemyslaw Szufel <pszufe@gmail.com>
2017-09-10 21:38:06 +0200

    a

---
+++ kissc-lib.sh
@@ -0,0 +1,52 @@
+function contains {
+    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
+}
+
+
+function checkinstall {
+  PKG_NAME=$1
+  PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${PKG_NAME}|grep "install ok installed")
+  if [ "" == "$PKG_OK" ]; then
+    echo "Missing package ${PKG_NAME}. "
+	echo "Trying to install ${PKG_NAME}. "
+    sudo apt --yes install $PKG_NAME
+  fi
+}
+
+
+function dynamoDBwait4table {
+    TABLENAME=$1
+    while
+        status=`aws dynamodb --region ${REGION} describe-table --table-name ${TABLENAME} 2>/dev/null  | jq -r ".Table.TableStatus"`
+		if [[ $status != "ACTIVE" ]]; then
+			echo "Waiting for DynamoDB table ${TABLENAME} in region ${REGION} to be active"
+			sleep 3
+		fi
+        [[ $status != "ACTIVE" ]]
+    do
+        :
+    done
+    echo "DynamoDB table ${TABLENAME} created"
+}
+
+function dynamoDBdroptable {
+    TABLENAME=$1
+	res=`aws dynamodb --region ${REGION} describe-table --table-name ${TABLENAME} 2>/dev/null` || echo "DynamoDB table ${TABLENAME} not found"
+	if [[ ! -z "${res// }" ]]; then
+	  echo "Dropping DynamoDB table ${TABLENAME}"
+	  res=`aws dynamodb --region ${REGION}  delete-table --table-name ${TABLENAME}`
+	  while
+	    res=`aws dynamodb --region ${REGION} describe-table --table-name ${TABLENAME} 2>/dev/null`
+		echo "Waiting for DynamoDB table ${TABLENAME} to be dropped"
+		sleep 3
+		if [[ ! -z "${res// }" ]]; then
+			echo "Waiting for DynamoDB table ${TABLENAME} to be dropped"
+			sleep 3
+		fi
+		[[ ! -z "${res// }" ]]
+	   do
+		 :
+	   done
+	   echo "DynamoDB table ${TABLENAME} has been dropped"
+	fi
+}
