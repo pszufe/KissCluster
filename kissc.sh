bbd4bd2d803b33bb8abaae693b606e4995012dea
Przemyslaw Szufel <pszufe@gmail.com>
2017-09-15 15:49:30 +0200

    a

--- kissc.sh
+++ kissc.sh
@@ -221,9 +221,12 @@ if [[ $COMMAND = "create" ]]; then
        dynamoDBwait4table kissc_clusters
     fi
 
-    echo "(re)setting the counters and configuration for ${CLUSTERNAME}"
+    echo "Setting the counters and configuration for ${CLUSTERNAME}"
 
-    
+	res=`aws dynamodb --region us-east-2 describe-table --table-name ${NODESTABLE} 2>/dev/null`
+    if [[ --n ${NODESTABLE} ]]; then
+		basic_usage 1 "The cluster ${CLUSTERNAME} already exist. Please use a different cluster name or delete the cluster first"
+	fi
 
     dynamoDBdroptable "${NODESTABLE} ${QUEUESTABLE} ${JOBSTABLE}"
 
