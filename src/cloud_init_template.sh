67c9cb837b6da4b80538a48becee783e686de8f4
Przemyslaw Szufel <pszufe@gmail.com>
2017-09-16 04:27:00 +0200

    a

--- src/cloud_init_template.sh
+++ src/cloud_init_template.sh
@@ -1,17 +1,14 @@
-
-
 sudo apt update --yes
 sudo apt install awscli jq --yes
-
 sudo su - ${USERNAME}
-
-
-[[ "${USERNAME}" = "root" ]] && USER_HOME=/root || USER_HOME=/home/${USERNAME}
-
+[[ "${USERNAME}" = "root" ]] && USER_HOME=/root || \
+    USER_HOME=/home/${USERNAME}
 HOME_DIR=${USER_HOME}/kissc-${CLUSTERNAME}
-aws  --region ${REGION} s3 cp ${S3_RUN_NODE_SCRIPT} ${HOMEDIR}/run_node_${CLUSTERNAME}.sh 
+aws  --region ${REGION} s3 cp ${S3_RUN_NODE_SCRIPT} \
+    ${HOMEDIR}/run_node_${CLUSTERNAME}.sh 
 cd ${HOME_DIR}
-
 echo "Now running the command:"
-echo "run_node_${CLUSTERNAME}.sh ${REGION} ${CLUSTERNAME} ${HOME_DIR} ${USERNAME} ${USER_HOME}"
-bash run_node_${CLUSTERNAME}.sh ${REGION} ${CLUSTERNAME} ${HOME_DIR} ${USERNAME} ${USER_HOME}
+echo "run_node_${CLUSTERNAME}.sh ${REGION} ${CLUSTERNAME} \
+    ${HOME_DIR} ${USERNAME} ${USER_HOME}"
+bash run_node_${CLUSTERNAME}.sh ${REGION} ${CLUSTERNAME} \
+    ${HOME_DIR} ${USERNAME} ${USER_HOME}
